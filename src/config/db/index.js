eb072e1d192b2404ee610220a77bf71773c4e46a
Vanish <sosvanish@gmail.com>
2023-11-18 21:54:02 +0700

    new update

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -1,19 +1,19 @@
 const mysql = require('mysql2')
 
-const connection = mysql.createConnection({
-    host: '127.0.0.1',
-    user: 'root',
-    password: 'vanish',
-    database: 'flight_booking'
-});
+// const connection = mysql.createConnection({
+//     host: '127.0.0.1',
+//     user: 'root',
+//     password: 'vanish',
+//     database: 'flight_booking'
+// });
 
-connection.connect(function(err){
-    if (err){
-        console.log("Kết nối DB thất bại")
-        console.log(err)
-    }else{
-        console.log("Kết nối DB thành công")
-    }
-})
+// connection.connect(function(err){
+//     if (err){
+//         console.log("Kết nối DB thất bại")
+//         console.log(err)
+//     }else{
+//         console.log("Kết nối DB thành công")
+//     }
+// })
 
 module.exports = null
