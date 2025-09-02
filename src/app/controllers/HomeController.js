eb52af3f404a65085236136d4ff0a867f3a76efe
anVanish <sosvanish@gmail.com>
2025-09-02 21:57:58 +0700

    add popular destination

--- src/app/controllers/HomeController.js
+++ src/app/controllers/HomeController.js
@@ -4,19 +4,27 @@ class HomeController {
     //GET / | /home
     async index(res, req, next) {
         const airports = await Airports.find({});
-        req.render("home", { airports: airports.map((a) => a.toObject()) });
-        // GeneralRepo.findPopBlog()
-        //     .then(([results]) =>{
-        //         var pops = results.map(item =>{
-        //             return {
-        //                 ...item,
-        //                 'public_date': item.public_date.toISOString().split('T')[0]
-        //             }
-        //         })
-        //         req.render('home', {
-        //             pops
-        //         })
-        //     })
+        const popularDestinations = [];
+        popularDestinations.push({
+            i: 1,
+            from: airports[3].toObject(),
+            to: airports[4].toObject(),
+        });
+        popularDestinations.push({
+            i: 2,
+            from: airports[4].toObject(),
+            to: airports[0].toObject(),
+        });
+        popularDestinations.push({
+            i: 3,
+            from: airports[1].toObject(),
+            to: airports[3].toObject(),
+        });
+
+        req.render("home", {
+            airports: airports.map((a) => a.toObject()),
+            popularDestinations,
+        });
     }
 }
 
