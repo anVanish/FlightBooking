31afd49c173683ba2e9be59a667794b05b09b16a
anVanish <sosvanish@gmail.com>
2025-09-02 08:57:47 +0700

    update user info

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,6 +1,5 @@
 const Users = require("../models/UserModel");
 const Tickets = require("../models/TicketModel");
-const Airports = require("../models/AirportModel");
 
 class UserController {
     //GET /user
@@ -14,8 +13,6 @@ class UserController {
                 populate: [{ path: "from" }, { path: "to" }],
             });
 
-        console.log(tickets);
-
         res.render("user", {
             user,
             tickets: tickets.map((t) => t.toObject()),
@@ -25,60 +22,40 @@ class UserController {
     //Get /user/edit
     edit(req, res, next) {
         var user = req.session.user;
-        // const date = new Date(user.date_of_birth);
-        // const formattedDate = date.toISOString().split("T")[0];
-        // user.date_of_birth = formattedDate;
 
         res.render("editUser", {
             user,
         });
     }
     //POST /user/edit
-    postEdit(req, res, next) {
-        var user = new User(req.body);
-        user.user_id = req.cookies.user.user_id;
+    async postEdit(req, res, next) {
+        try {
+            const user = req.session.user;
+            const { name, email, phone } = req.body;
 
-        //Validation
-        const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-        const phone_regex = /^\d{10}$/;
-        if (user.name === "") {
-            res.send("Tên người dùng không thể để trống");
-            return;
-        } else if (user.email === "") {
-            res.send("Email không thể để trống");
-            return;
-        } else if (!regex.test(user.email)) {
-            res.send("Email không hợp lệ");
-            return;
-        } else if (
-            !phone_regex.test(user.phone_number) &&
-            user.phone_number !== ""
-        ) {
-            res.send("Số điện thoại không hợp lệ");
-            return;
-        } else if (
-            new Date(user.date_of_birth) >=
-            new Date(Date.now() - 12 * 365 * 24 * 60 * 60 * 1000)
-        ) {
-            res.send("Ngày sinh không hợp lệ! Độ tuổi phải lớn hơn 12");
-            return;
+            //Validation
+            const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+            const phoneRegex = /^\d{10}$/;
+            if (!name) throw new Error("Tên người dùng không thể để trống");
+            if (!email) throw new Error("Email không thể để trống");
+            if (!regex.test(email)) throw new Error("Email không hợp lệ");
+            if (!phoneRegex.test(phone) && phone !== "")
+                throw new Error("Số điện thoại không hợp lệ");
+
+            const updatedUser = await Users.findOneAndUpdate(
+                { _id: user._id },
+                { name, email, phone },
+                { new: true }
+            );
+            if (!updatedUser) throw new Error("Cập nhật thất bại");
+            let safeUser = updatedUser.toObject();
+            delete safeUser.password;
+
+            req.session.user = safeUser;
+            res.send("Cập nhật thành công");
+        } catch (err) {
+            next(err);
         }
-
-        //update
-        // UserRepo.update(user)
-        //     .then((result) => {
-        //         res.clearCookie("user");
-        //         res.cookie("user", user, {
-        //             expires: new Date(Date.now() + 900000),
-        //         });
-        //         req.session.user = user;
-
-        //         res.send("");
-        //     })
-        //     .catch((err) => {
-        //         res.send("Cập nhật thất bại");
-        //         console.log(err);
-        //     });
     }
 }
 
--- src/app/models/UserModel.js
+++ src/app/models/UserModel.js
@@ -1,16 +1,3 @@
-// const User = function(user){
-//     this.user_id = user.user_id || -1
-//     this.user_name = user.user_name || ''
-//     this.email = user.email || ''
-//     this.password = user.password || ''
-//     this.date_of_birth = user.date_of_birth || null
-//     this.address = user.address || ''
-//     this.phone_number = user.phone_number || ''
-//     this.gender = user.gender || ''
-// }
-
-// module.exports = User
-
 const mongoose = require("mongoose");
 
 const Users = new mongoose.Schema(
@@ -18,7 +5,7 @@ const Users = new mongoose.Schema(
         name: { type: String, required: true },
         email: { type: String, required: true },
         password: { type: String, required: true },
-        phoneNumber: { type: String },
+        phone: { type: String },
         isAdmin: { type: Boolean, default: false },
     },
     {
